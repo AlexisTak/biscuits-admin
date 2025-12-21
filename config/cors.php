e62cfc1444956f469391eea83919d6abb36cd18d
AlexisTak <swetoksy@gmail.com>
2025-12-21 02:30:02 +0100

    function disable for fetch

--- config/cors.php
+++ config/cors.php
@@ -1,24 +1,26 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['POST', 'GET', 'OPTIONS'],
 
     'allowed_origins' => [
+        'https://biscuits-ia.com',
         'http://localhost:4321',
         'http://127.0.0.1:4321',
-        'https://biscuits.dev',
-        'https://www.biscuits.dev',
     ],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => [
+        'Content-Type',
+        'Accept',
+    ],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
